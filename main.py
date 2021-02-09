13a19e22d0a8ccad537a699dd9ba26d4507a3167
h-pgy <hpougy@gmail.com>
2021-02-09 08:37:54 -0300

    agora com controle do comportamento via query string (?json_alike=False)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from typing import Optional
 from utilities.my_tools import get_proc_aleatorio, get_proc
 from utilities.proj_decorators import treat_proc_num_out_of_pattern, treat_proc_not_found
 from process_data.responsavel_imovel import dados_resps_imovel
@@ -9,12 +8,11 @@ app = FastAPI()
 @app.get("/responsavel_imovel/{num_proc}")
 @treat_proc_not_found
 @treat_proc_num_out_of_pattern
-def dados_responsavel(num_proc: str):
+def dados_responsavel(num_proc: str, json_alike : bool = True):
 
-    #PRECISO ENTENDER OS QUERY PARAMETERS - NAO ESTA SE COMPORTANDO COMO DEVERIA
     if num_proc:
         p = get_proc(num_proc)
     else:
         p = get_proc_aleatorio()
 
-    return dados_resps_imovel(p, json_alike = True)
+    return dados_resps_imovel(p, json_alike = json_alike)
