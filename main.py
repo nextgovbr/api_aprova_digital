c91f714fee37289eaad379cfae5373c4db9f7d9c
h-pgy <hpougy@gmail.com>
2021-02-09 08:41:45 -0300

    making json_alike optional parameter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from typing import Optional
 from utilities.my_tools import get_proc_aleatorio, get_proc
 from utilities.proj_decorators import treat_proc_num_out_of_pattern, treat_proc_not_found
 from process_data.responsavel_imovel import dados_resps_imovel
@@ -8,7 +9,10 @@ app = FastAPI()
 @app.get("/responsavel_imovel/{num_proc}")
 @treat_proc_not_found
 @treat_proc_num_out_of_pattern
-def dados_responsavel(num_proc: str, json_alike : bool = True):
+def dados_responsavel(num_proc: str, json_alike: Optional[bool] = None):
+
+    if json_alike is None:
+        json_alike = True
 
     if num_proc:
         p = get_proc(num_proc)
