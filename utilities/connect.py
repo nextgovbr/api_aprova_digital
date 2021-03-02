d5671dd90a3ab67ca496a5a77b3d70a695324edc
Renan Tashiro <renantashiro@htomail.com>
2021-03-02 09:37:38 -0300

    refactor(): troca variaveis de ambiente em arquivo por variavel de ambiente

--- utilities/connect.py
+++ utilities/connect.py
@@ -1,8 +1,8 @@
+import os
 from pymongo import MongoClient
-from config import conn_string
 
 
-def gen_db(conn_string=conn_string):
+def gen_db(conn_string=os.environ['conn_string']):
 
     client = MongoClient(conn_string)
     db = client['next-producao']
